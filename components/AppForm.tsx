280ee4e65e0670123e5664ab77e0e0f131ffe500
vishnumohanrk <mohanvish1999@gmail.com>
2021-01-03 20:37:14 +0530

    max-w

--- components/AppForm.tsx
+++ components/AppForm.tsx
@@ -33,7 +33,7 @@ const AppForm = ({ onSubmit, shouldDisable }: AppFormProps) => {
         disabled={shouldDisable}
         className={`select-none text-white font-medium rounded-md transition-colors mt-3 px-5 py-2 bg-blue-600 ${
           shouldDisable
-            ? 'opacity-80 cursor-not-allowed'
+            ? 'opacity-50 cursor-not-allowed'
             : 'hover:bg-blue-500 focus:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-500 focus:ring-offset-white focus:ring-offset-2'
         }`}
       >
--- components/CardContainer.tsx
+++ components/CardContainer.tsx
@@ -7,7 +7,13 @@ interface CardContainerProps {
 
 const CardContainer = ({ dataList }: CardContainerProps) => (
   <div
-    className={`grid items-stretch my-4 max-w-xs sm:max-w-2xl mx-auto gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 lg:max-w-5xl`}
+    className={`grid items-stretch my-4 max-w-xs mx-auto gap-8 grid-cols-1 ${
+      dataList.length === 1
+        ? ''
+        : dataList.length === 2
+        ? 'sm:max-w-2xl sm:grid-cols-2'
+        : 'sm:max-w-2xl sm:grid-cols-2 lg:grid-cols-3 lg:max-w-5xl'
+    }`}
   >
     {dataList.map(i => (
       <Card {...i} key={i.id} />
